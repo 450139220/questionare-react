70640b4945ac82005f7060095a8a9695cb547e93
CHAM <450139220@qq.com>
2025-11-18 22:22:13 +0800

    feat: navigation

--- src/views/dashboard/Operations.tsx
+++ src/views/dashboard/Operations.tsx
@@ -1,18 +1,20 @@
 import Button from "@/components/basic/Button";
+import { useNavigate } from "react-router";
 
 export default function Operations() {
+    const navigate = useNavigate();
     return (
         <div className="mx-4 mt-4 flex gap-2">
             <Button
                 onClick={() => {
-                    console.log("goto create");
+                    navigate("/create");
                 }}
             >
                 Create
             </Button>
             <Button
                 onClick={() => {
-                    console.log("goto market");
+                    navigate("/market");
                 }}
             >
                 Market
